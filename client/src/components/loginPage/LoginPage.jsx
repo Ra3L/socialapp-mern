16c0c9cd18823ff8a66b876e34024240a76461da
RhaeL <rhaeliang@gmail.com>
2024-02-07 15:35:27 +1100

    update theme

--- client/src/components/loginPage/LoginPage.jsx
+++ client/src/components/loginPage/LoginPage.jsx
@@ -6,15 +6,16 @@ const LoginPage = () => {
   const theme = useTheme();
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   return (
-    <Box>
-      <Box
-        width="100%"
-        backgroundColor={theme.palette.background.alt}
-        p="1rem 6%"
-        textAlign="center"
-      >
-        <Typography fontWeight="bold" fontSize="32px" color="primary">
-          SocialApp
+    <Box
+      style={{
+        backgroundImage: `url(${process.env.REACT_APP_API_URL}/assets/footballs.jpg)`,
+        backgroundSize: "cover",
+        height: "100vh",
+      }}
+    >
+      <Box width="100%" p="6rem 6% 1rem 6%" textAlign="center">
+        <Typography fontWeight="bold" fontSize="40px" color="primary">
+          Dribbling
         </Typography>
       </Box>
 
@@ -26,7 +27,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to SocialApp!
+          Welcome to Dribbling!
         </Typography>
         <Form />
       </Box>
--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
             },
           }}
         >
-          SocialApp
+          Dribbling
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
